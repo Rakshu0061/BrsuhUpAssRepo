4999082217ad9bcd949c4838fd4ef806f3484a51
Rakshu0061 <rakshanda.shinde96@gmail.com>
2024-10-24 13:35:21 +0530

    Add Substraction

--- Substraction.java
+++ Substraction.java
@@ -2,6 +2,6 @@ public class Substraction {
     public static void main(String[] args) {
         int a=20;
         int b=10;
-        System.out.println(a-b);
+        System.out.println(a+b);
     }
 }
