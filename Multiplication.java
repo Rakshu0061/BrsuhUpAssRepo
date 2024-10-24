7e07aedf24e9ae82f26a43ace11d8f0473d46446
Rakshu0061 <rakshanda.shinde96@gmail.com>
2024-10-24 13:39:55 +0530

    Add Multiplication

--- Multiplication.java
+++ Multiplication.java
@@ -2,6 +2,6 @@ public class Multiplication {
     public static void main(String[] args) {
         int a=10;
         int b=10;
-        System.out.println("Addition "+(a+b));
+        System.out.println("Addition "+(a/b));
     }
 }
